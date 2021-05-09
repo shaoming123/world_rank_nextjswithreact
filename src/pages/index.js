8f9aed170ac3f6f029a97bbd147f9ff9be31e765
shaoming123 <greatfoodprefer@gmail.com>
2021-05-09 16:34:10 +0800

    first

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import CountryTable from "../components/CountryTable/CountryTable";
+import Layout from "../components/layout/layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} coutries </div>
+
+        <SearchInput placeholder="input" onChange={onInputChange} />
+      </div>
+      <CountryTable countries={filteredCountries} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
